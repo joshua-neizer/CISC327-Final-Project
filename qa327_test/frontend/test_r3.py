e92265e4835c46e424c00372f744cc3ba0de97dd
Ted Brownlow <ted.brownlow@gmail.com>
2020-11-11 15:01:37 -0500

    implemented r3.2 test

--- qa327_test/frontend/test_r3.py
+++ qa327_test/frontend/test_r3.py
@@ -1,8 +1,21 @@
 import pytest
 from seleniumbase import BaseCase
-
-from qa327_test.conftest import base_url
 from unittest.mock import patch
+from qa327_test.conftest import base_url
+from qa327.models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Moch a sample user
+test_user = User(
+    email='test_frontend@test.com',
+    name='test_frontend',
+    password=generate_password_hash('test_frontend')
+)
+
+# Moch some sample tickets
+test_tickets = [
+    {'name': 't1', 'price': '100'}
+]
 
 class R3Test(BaseCase):
 
@@ -15,3 +28,12 @@ class R3Test(BaseCase):
         # verify redirected to /login
         assert self.get_current_url() == base_url+'/login'
         self.assert_element('#login_input')
+
+    @patch('qa327.backend.get_user', return_value=test_user)
+    def test_hi_user(self,*_):
+        '''
+        see r3.2
+        '''
+        # open home page
+        self.open(base_url)
+        self.assert_text('Hi test_frontend','#header')
