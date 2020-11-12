1b2bcdf013ad798d1a400d538f2281a84c007aeb
Ted Brownlow <ted.brownlow@gmail.com>
2020-11-11 20:25:44 -0500

    applied auto pep8

--- qa327_test/frontend/test_r3.py
+++ qa327_test/frontend/test_r3.py
@@ -2,16 +2,15 @@
 Tests requirements according to R3
 '''
 
-from seleniumbase import BaseCase
 from unittest.mock import patch
-import pytest
+from seleniumbase import BaseCase
 from werkzeug.security import generate_password_hash
 
 from qa327_test.conftest import base_url
 from qa327.models import User
 
 # Moch a sample user
-test_user = User(
+TEST_USER = User(
     email='test_frontend@test.com',
     name='test_frontend',
     password=generate_password_hash('test_frontend'),
@@ -19,10 +18,11 @@ test_user = User(
 )
 
 # Moch some sample tickets
-test_tickets = [
-    {'name': 't1', 'price': '100', 'owner':'god', 'count':2},
-    {'name': 't2', 'price': '90', 'owner':'geek', 'count':3},
+TEST_TICKETS = [
+    {'name': 't1', 'price': '100', 'owner': 'god', 'count': 2},
+    {'name': 't2', 'price': '90', 'owner': 'geek', 'count': 3},
 ]
+
 
 class R3Test(BaseCase):
     '''
@@ -30,9 +30,10 @@ class R3Test(BaseCase):
     '''
 
     def login_test_user(self):
+        '''login our test user'''
         self.open(base_url+'/login')
-        self.input('#email',test_user.email)
-        self.input('#password','test_frontend')
+        self.input('#email', TEST_USER.email)
+        self.input('#password', 'test_frontend')
         self.click('#btn-submit')
 
     def test_login_redirects(self, *_):
@@ -42,8 +43,8 @@ class R3Test(BaseCase):
         assert self.get_current_url() == base_url+'/login'
         self.assert_element('#login_message')
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_hi_user(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_hi_user(self, *_):
         '''see r3.2'''
         self.login_test_user()
         self.open(base_url)
@@ -51,39 +52,42 @@ class R3Test(BaseCase):
         # balance
         assert 'Hi test_frontend' in self.find_element('#welcome-header').text
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_user_balance(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_user_balance(self, *_):
         '''see r3.3'''
         self.login_test_user()
         self.open(base_url)
         # use contains check because element also contains username
-        assert 'Your balance is $140' in self.find_element('#welcome-header').text
+        assert 'Your balance is $140' in self.find_element(
+            '#welcome-header').text
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_logout_link(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_logout_link(self, *_):
         '''see r3.4'''
         self.login_test_user()
         self.open(base_url)
         self.assert_element('#logout')
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    @patch('qa327.backend.get_all_tickets', return_value=test_tickets)
-    def test_tickets_listed(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    @patch('qa327.backend.get_all_tickets', return_value=TEST_TICKETS)
+    def test_tickets_listed(self, *_):
         '''see r3.5'''
         self.login_test_user()
-        for ticket in test_tickets:
+        for ticket in TEST_TICKETS:
             name = ticket['name']
             # find div containing ticket info in page
             ticket_div = self.find_element(f'#tickets .ticket[name={name}]')
             # verify all ticket properties match mock
-            for key in ticket.keys():
-                displayed_text=ticket_div.find_element_by_class_name(key).text
+            for prop, value in ticket.items():
+                displayed_text = ticket_div.find_element_by_class_name(
+                    prop
+                ).text
                 # assert that displayed ticket property matches
                 # mock ticket
-                self.assertEqual(displayed_text,str(ticket[key]))
+                self.assertEqual(displayed_text, str(value))
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_sell_form(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_sell_form(self, *_):
         '''see r3.6'''
         self.login_test_user()
         self.open(base_url)
@@ -92,16 +96,16 @@ class R3Test(BaseCase):
         self.assert_element('#sell-ticket-price')
         self.assert_element('#sell-ticket-expiration-date')
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_buy_form(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_buy_form(self, *_):
         '''see r3.7'''
         self.login_test_user()
         self.open(base_url)
         self.assert_element('#buy-ticket-name')
         self.assert_element('#buy-ticket-quantity')
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_update_form(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_update_form(self, *_):
         '''see r3.8'''
         self.login_test_user()
         self.open(base_url)
@@ -110,36 +114,36 @@ class R3Test(BaseCase):
         self.assert_element('#update-ticket-price')
         self.assert_element('#update-ticket-expiration-date')
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_sell_posts(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_sell_posts(self, *_):
         '''see r3.9'''
         self.login_test_user()
         self.open(base_url)
-        self.input('#sell-ticket-name','dont-care')
-        self.input('#sell-ticket-quantity','dont-care')
-        self.input('#sell-ticket-price','dont-care')
-        self.input('#sell-ticket-expiration-date','dont-care')
+        self.input('#sell-ticket-name', 'dont-care')
+        self.input('#sell-ticket-quantity', 'dont-care')
+        self.input('#sell-ticket-price', 'dont-care')
+        self.input('#sell-ticket-expiration-date', 'dont-care')
         self.click('#sell-submit')
-        self.assert_text('ticket sold successfully','#post-message')
+        self.assert_text('ticket sold successfully', '#post-message')
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_buy_posts(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_buy_posts(self, *_):
         '''see r3.10'''
         self.login_test_user()
         self.open(base_url)
-        self.input('#buy-ticket-name','dont-care')
-        self.input('#buy-ticket-quantity','dont-care')
+        self.input('#buy-ticket-name', 'dont-care')
+        self.input('#buy-ticket-quantity', 'dont-care')
         self.click('#buy-submit')
-        self.assert_text('ticket bought successfully','#post-message')
+        self.assert_text('ticket bought successfully', '#post-message')
 
-    @patch('qa327.backend.get_user', return_value=test_user)
-    def test_update_posts(self,*_):
+    @patch('qa327.backend.get_user', return_value=TEST_USER)
+    def test_update_posts(self, *_):
         '''see r3.11'''
         self.login_test_user()
         self.open(base_url)
-        self.input('#update-ticket-name','dont-care')
-        self.input('#update-ticket-quantity','dont-care')
-        self.input('#update-ticket-price','dont-care')
-        self.input('#update-ticket-expiration-date','dont-care')
+        self.input('#update-ticket-name', 'dont-care')
+        self.input('#update-ticket-quantity', 'dont-care')
+        self.input('#update-ticket-price', 'dont-care')
+        self.input('#update-ticket-expiration-date', 'dont-care')
         self.click('#update-submit')
-        self.assert_text('ticket updated successfully','#post-message')
+        self.assert_text('ticket updated successfully', '#post-message')
