6253b9299a246761c5005dfab5533cdc3fb0a63f
Ted Brownlow <ted.brownlow@gmail.com>
2020-11-16 21:16:04 -0500

    pytest uses different port

--- qa327_test/conftest.py
+++ qa327_test/conftest.py
@@ -9,14 +9,17 @@ from qa327.__main__ import app
 import threading
 from werkzeug.serving import make_server
 
+# separate port allows tests to be run while hosting
+# actual app
+FLASK_TEST_PORT = 8082
 
-base_url = 'http://localhost:{}'.format(FLASK_PORT)
+base_url = 'http://localhost:{}'.format(FLASK_TEST_PORT)
 
 class ServerThread(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
-        self.srv = make_server('127.0.0.1', FLASK_PORT, app)
+        self.srv = make_server('127.0.0.1', FLASK_TEST_PORT, app)
         self.ctx = app.app_context()
         self.ctx.push()
 
