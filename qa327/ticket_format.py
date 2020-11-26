617b38794a8b32736f7414ad2503e2e93df0ae07
Joshua Neizer <joshua.neizer@gmail.com>
2020-11-26 12:28:23 -0500

    Code now follows PEP 8 standards

--- qa327/ticket_format.py
+++ qa327/ticket_format.py
@@ -7,7 +7,7 @@ def is_valid_ticket_name(ticket_name):
     Validate ticket name with provided requirements
     :param ticket_name: ticket name of ticket trying to be registered
     :return: true if the username is acceptable, false otherwise
-    """    
+    """
     return (
         # ticket name has at least 6 characters, but no greater than 60
         6 <= len(ticket_name) <= 60 and
@@ -17,6 +17,7 @@ def is_valid_ticket_name(ticket_name):
         # is alphanumeric (plus space)
         bool(re.match(r'^[\w\d ]+$', ticket_name))
     )
+
 
 def is_valid_quantity(quantity):
     """
@@ -29,8 +30,9 @@ def is_valid_quantity(quantity):
         quantity = int(quantity)
         # Quantity is greater than 0 and less than 100
         return 0 < quantity <= 100
-    except:
+    except ValueError:
         return False
+
 
 def is_valid_price(price):
     """
@@ -43,8 +45,9 @@ def is_valid_price(price):
         price = int(price)
         # Price must be at least $10 but no more than $100
         return 10 <= price <= 100
-    except:
+    except ValueError:
         return False
+
 
 def is_valid_date(date):
     """
@@ -58,5 +61,5 @@ def is_valid_date(date):
         date = datetime.strptime(date, date_format)
         # Date must not have already passed
         return datetime.now() < date
-    except:
+    except ValueError:
         return False
