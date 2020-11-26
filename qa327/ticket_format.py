b7b1ce9af34bf8de002dd6175890dfa911bbb094
Joshua Neizer <joshua.neizer@gmail.com>
2020-11-26 11:34:47 -0500

    Created outline for ticket_format.py

---
+++ qa327/ticket_format.py
@@ -0,0 +1,14 @@
+"""helper functions for ticket format verification"""
+import re
+
+def is_valid_ticket_name(ticket_name):
+    pass
+
+def is_valid_quantity(quantity):
+    pass
+
+def is_valid_price(price):
+    pass
+
+def is_valid_date(date):
+    pass
