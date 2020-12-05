c3670eae4a9dd7326f020397c2d3d3295872450a
16cmc21@queensu.ca <16cmc21@queensu.ca>
2020-12-05 17:12:21 -0500

    all test cases passing

--- qa327_test/frontend/test_r4.py
+++ qa327_test/frontend/test_r4.py
@@ -18,6 +18,8 @@ GOOD_TICKET = Ticket(
     expires="20220101"
 )
 
+GOOD_TICKET_DICT = [{'name': 'helloworld','price': '20','owner': 'jesus', 'count': '20'}]
+
 INVALID_NAME_FORMATS = ['bad',' alsobad', 'alsobad ', '$alsobad$',
     'veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerylongname'
 ]
@@ -99,20 +101,30 @@ class R4Test(GeekBaseCase):
             self.assert_flash('Invalid ticket date')
 
     @patch('qa327.backend.get_user', return_value=TEST_USER)
-    def test_good_ticket(self,*_):
+    @patch('qa327.backend.sell_ticket', return_value='ticket sold successfully')
+    @patch('qa327.backend.get_all_tickets')
+    def test_good_ticket(self, get_all_tickets_function, *_):
         '''
         See r4.6
         Ticket with proper format will be posted to profile page
         '''
         self.login_test_user()
         self.open(base_url)
+
+        get_all_tickets_function.return_value = []
+
         self.input('#sell-ticket-name', GOOD_TICKET.name)
         self.input('#sell-ticket-quantity', GOOD_TICKET.quantity)
         self.input('#sell-ticket-price', GOOD_TICKET.price)
         self.input('#sell-ticket-expiration-date', GOOD_TICKET.expires)
         self.click('#sell-submit')
+        self.assert_flash('ticket sold successfully')
+
+        get_all_tickets_function.return_value = GOOD_TICKET_DICT
+        self.refresh()
         name = GOOD_TICKET.name
+
         ticket_div = self.find_element(f'#tickets .ticket[name={name}]')
-        for prop, value in GOOD_TICKET.items():
+        for prop, value in GOOD_TICKET_DICT[0].items():
             displayed_text = ticket_div.find_element_by_class_name(prop).text
             self.assertEqual(displayed_text, str(value))
